414734fc1bd65492db4a28007cc41cdd8ac7f9a9
ian-daniel132 <123045412@upq.edu.mx>
2025-09-18 20:03:08 -0600

    primer egercicio de la practica 1
    cambiamos el uso del var por el let para poder hacer el cambio de los valores de las variables nombre y edad.

---
+++ Practica No 1/Practica_No_1.java
@@ -0,0 +1,11 @@
+
+
+//Se usa let en lugar para poder cambiar el valor de las variables 
+//para que de esta forma se muestre la nueva informacion 
+
+Let nombre = "Armando";
+Let edad = 25; 
+nombre = "Ana Maria";
+edad = 30; 
+const saludo = 'Hola, ${nombre}, Tinenes ${edad} años';
+console.log(saludo)
