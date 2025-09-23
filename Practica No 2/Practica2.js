56e7be4d9cbe9fd64409e638c623faba6d09f79a
ian-daniel132 <123045412@upq.edu.mx>
2025-09-22 19:15:44 -0600

    Ejercicio 2 de la segunda practica

--- Practica No 2/Practica2.js
+++ Practica No 2/Practica2.js
@@ -1,8 +1,14 @@
 
-const persona = { 
-    nombre : " Daniel",
-    edad : "21",
-    ciudad : "Queretaro"
-};
-const { nombre, edad, ciudad } = persona;
-console.log("Me llamo:", + nombre + ", tengo " + edad + " años de edad y vivo en " + ciudad );
+const productos = [ 
+    { nombre: "Laptop", precio: 1500 },
+    { nombre: "Teclado", precio: 1200},
+    { nombre: "Mouse", precio: 50},
+    { nombre: "Monitor", precio: 300},
+    { nombre: "Tablet", precio: 800}
+]; 
+
+const productosCaros = productos
+    .filter(producto => producto.precio > 1000)
+    .map(producto => producto.nombre); 
+
+console.log(productosCaros); 
