75db7b5ab7830f0b874b8851dc1fa333b6fa5ab8
ian-daniel132 <123045412@upq.edu.mx>
2025-09-22 19:07:24 -0600

    ejercicio 1 de la segunda practica.

---
+++ Practica No 2/Practica2.js
@@ -0,0 +1,8 @@
+
+const persona = { 
+    nombre : " Daniel",
+    edad : "21",
+    ciudad : "Queretaro"
+};
+const { nombre, edad, ciudad } = persona;
+console.log("Me llamo:", + nombre + ", tengo " + edad + " años de edad y vivo en " + ciudad );
