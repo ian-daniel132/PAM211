ac6f08ca28838adaebf0d44037b4d6453502888d
ian-daniel132 <123045412@upq.edu.mx>
2025-09-25 20:08:20 -0600

    Ejercicio1 3er Practica

---
+++ Practica No 3/MainEjercicio1.js
@@ -0,0 +1,8 @@
+
+import { restar } from './Ejercicio1Practica3.js';
+
+console.log("=== PRUEBAS DE LA FUNCIÓN RESTAR ===");
+console.log("10 - 5 = " + restar(10, 5));
+console.log("8 - 3 = " + restar(8, 3));
+console.log("15 - 7 = " + restar(15, 7));
+console.log("5 - 10 = " + restar(5, 10));
